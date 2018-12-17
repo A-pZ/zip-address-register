ef7359ef4a91c4d3fac4127876e06f6bd605d901
A-pZ <g.alpha.pz@gmail.com>
2018-12-17 21:09:39 +0900

    Deployable war and runtime version fix.

--- src/main/java/com/github/apz/AddressRegisterApplication.java
+++ src/main/java/com/github/apz/AddressRegisterApplication.java
@@ -2,13 +2,18 @@ package com.github.apz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AddressRegisterApplication {
+public class AddressRegisterApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AddressRegisterApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AddressRegisterApplication.class);
+	}
 }
-
